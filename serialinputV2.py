312677de3598aa8fc3e27736295a4f650cd8c69d
hornerlas <sam@samhorner.co.uk>
2013-02-19 14:59:13 -0800

    add volume itunes code

--- serialinputV2.py
+++ serialinputV2.py
@@ -9,7 +9,7 @@ while 1:
 	if val == "A"
 		os.system('''osascript -e 'tell application "iTunes" to playpause' ''')
 	else:
-		('Something else')
+		os.system('''osascript -e 'set sound volume to (sound volume - 25)' ''')
 s.close()
 
 
